ddec689a4b7991c97776e835ab98a5b5f12460c3
Aritra Bhakt <arre2barre@gmail.com>
2023-09-13 17:23:33 +0200

    Add wheelchair controller button interaction

--- Assets/Scripts/WheelchairController.cs
+++ Assets/Scripts/WheelchairController.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class WheelchairController : MonoBehaviour
 {
     public AxleInfo axleInfo; // the information about each individual axle
     public float maxTorque; // maximum torque the motor can apply to wheel
+
+    private Hand hand;
+
+    private void Start()
+    {
+        hand = GetComponent<Hand>();
+    }
 
     // finds the corresponding visual wheel
     // correctly applies the transform
@@ -36,21 +47,21 @@ public class WheelchairController : MonoBehaviour
 
     void FixedUpdate()
     {
-        //float torqueLeft = maxTorque * Input.GetAxis("Left wheel");
-        //float torqueRight = maxTorque * Input.GetAxis("Right wheel");
+        SteamVR_Action_Boolean state = SteamVR_Input.GetBooleanAction("GrabGrip");
+        Debug.Log(state.GetState(SteamVR_Input_Sources.Any));
+        float torqueLeft = maxTorque * Convert.ToInt32(state.GetState(SteamVR_Input_Sources.LeftHand));
+        float torqueRight = maxTorque * Convert.ToInt32(state.GetState(SteamVR_Input_Sources.RightHand));
 
-        //axleInfo.leftWheel.motorTorque = torqueLeft;
-        //axleInfo.rightWheel.motorTorque = torqueRight;
-
-        //Debug.Log(torqueLeft + " " + torqueRight);
+        axleInfo.leftWheel.motorTorque = torqueLeft;
+        axleInfo.rightWheel.motorTorque = torqueRight;
     }
-}
 
-[System.Serializable]
-public class AxleInfo
-{
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    public WheelCollider leftSupport;
-    public WheelCollider rightSupport;
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public WheelCollider leftSupport;
+        public WheelCollider rightSupport;
+    }
 }
