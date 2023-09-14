beb977e89d55483fda84c72db6459c15f05f8383
Aritra Bhakt <arre2barre@gmail.com>
2023-09-14 12:48:34 +0200

    Add demo scene, wheelchair physical controls

--- Assets/Scripts/WheelchairController.cs
+++ Assets/Scripts/WheelchairController.cs
@@ -10,12 +10,14 @@ public class WheelchairController : MonoBehaviour
 {
     public AxleInfo axleInfo; // the information about each individual axle
     public float maxTorque; // maximum torque the motor can apply to wheel
+    public float maxBrakeTorque;
 
-    private Hand hand;
+    private Rigidbody rb;
 
     private void Start()
     {
-        hand = GetComponent<Hand>();
+        rb = this.GetComponent<Rigidbody>();
+
     }
 
     // finds the corresponding visual wheel
@@ -39,29 +41,48 @@ public class WheelchairController : MonoBehaviour
 
     void Update()
     {
-        ApplyLocalPositionToVisuals(axleInfo.leftWheel);
-        ApplyLocalPositionToVisuals(axleInfo.rightWheel);
-        ApplyLocalPositionToVisuals(axleInfo.leftSupport);
-        ApplyLocalPositionToVisuals(axleInfo.rightSupport);
+        ApplyLocalPositionToVisuals(axleInfo.wheels[(int)Wheels.RearLeft]);
+        ApplyLocalPositionToVisuals(axleInfo.wheels[(int)Wheels.RearRight]);
+        ApplyLocalPositionToVisuals(axleInfo.wheels[(int)Wheels.CasterLeft]);
+        ApplyLocalPositionToVisuals(axleInfo.wheels[(int)Wheels.CasterRight]);
     }
 
     void FixedUpdate()
     {
-        SteamVR_Action_Boolean state = SteamVR_Input.GetBooleanAction("GrabGrip");
-        Debug.Log(state.GetState(SteamVR_Input_Sources.Any));
-        float torqueLeft = maxTorque * Convert.ToInt32(state.GetState(SteamVR_Input_Sources.LeftHand));
-        float torqueRight = maxTorque * Convert.ToInt32(state.GetState(SteamVR_Input_Sources.RightHand));
+        SteamVR_Action_Pose pose = SteamVR_Input.GetPoseAction("Pose");
+        SteamVR_Action_Boolean grip = SteamVR_Input.GetBooleanAction("GrabGrip");
 
-        axleInfo.leftWheel.motorTorque = torqueLeft;
-        axleInfo.rightWheel.motorTorque = torqueRight;
+        SteamVR_Input_Sources[] inputHands = { SteamVR_Input_Sources.LeftHand, SteamVR_Input_Sources.RightHand };
+        for (int i = 0; i < 2; i++)
+        {
+            float torque = 0;
+            float brakeTorque = 0;
+            SteamVR_Input_Sources inputHand = inputHands[i];
+            if (grip.GetState(inputHand))
+            {
+                float velocity = pose.GetVelocity(inputHand).z;
+                if (Math.Abs(velocity) > 0.1f)
+                {
+                    torque = maxTorque * velocity;
+                }
+                else
+                {
+                    brakeTorque = maxBrakeTorque;
+                }
+                axleInfo.wheels[i].brakeTorque = brakeTorque;
+                axleInfo.wheels[i].motorTorque = torque;
+            }
+        }
     }
+}
 
-    [System.Serializable]
-    public class AxleInfo
-    {
-        public WheelCollider leftWheel;
-        public WheelCollider rightWheel;
-        public WheelCollider leftSupport;
-        public WheelCollider rightSupport;
-    }
+enum Wheels
+{
+    RearLeft, RearRight, CasterLeft, CasterRight
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider[] wheels = new WheelCollider[4];
 }
