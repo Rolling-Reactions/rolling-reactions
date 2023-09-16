e7062e1dc9711fedcfab47f06bc9f2178d5f4d35
Aritra Bhakt <arre2barre@gmail.com>
2023-09-16 14:23:34 +0200

    fixing control and many packages

--- Assets/Scripts/WheelchairController.cs
+++ Assets/Scripts/WheelchairController.cs
@@ -58,21 +58,53 @@ public class WheelchairController : MonoBehaviour
             float torque = 0;
             float brakeTorque = 0;
             SteamVR_Input_Sources inputHand = inputHands[i];
-            if (grip.GetState(inputHand))
-            {
-                float velocity = pose.GetVelocity(inputHand).z;
-                if (Math.Abs(velocity) > 0.1f)
+            if (grip.GetStateUp(inputHand)){GetVelocity(inputHand, i); }
+            if (grip.GetStateDown(inputHand)) {
+                if (pose.GetVelocity(inputHand).z > -0.1 && pose.GetVelocity(inputHand).z < 0.1)
                 {
-                    torque = maxTorque * velocity;
+                    axleInfo.wheels[i].wheelDampingRate = 20;
                 }
-                else
-                {
-                    brakeTorque = maxBrakeTorque;
-                }
-                axleInfo.wheels[i].brakeTorque = brakeTorque;
-                axleInfo.wheels[i].motorTorque = torque;
             }
+
         }
+    }
+    void GetVelocity(SteamVR_Input_Sources inputHand, int i)
+    {
+        Debug.Log("here");
+        float highestVelocity = 0f;
+        SteamVR_Action_Boolean grip = SteamVR_Input.GetBooleanAction("GrabGrip");
+        SteamVR_Action_Pose pose = SteamVR_Input.GetPoseAction("Pose");
+
+
+        //float velocity = pose.GetVelocity(inputHand).z;
+        //if (Math.Abs(velocity) > 0.1f)
+        //{
+        //    Debug.Log(axleInfo.wheels[1].rpm);
+        //    torque = maxTorque;
+
+        //}
+        //else
+        //{
+
+        //    axleInfo.wheels[i].brakeTorque = brakeTorque;
+        //}
+
+        //axleInfo.wheels[i].motorTorque = torque;
+        if(pose.GetVelocity(inputHand).z < -0.1)
+        {
+            highestVelocity = -0.2f;
+            axleInfo.wheels[i].wheelDampingRate = 0.25f;
+        }
+        else if(pose.GetVelocity(inputHand).z > 0.1)
+        {
+            highestVelocity = 0.2f;
+            axleInfo.wheels[i].wheelDampingRate = 0.25f;
+        }
+ 
+        
+
+        axleInfo.wheels[i].motorTorque = maxTorque * highestVelocity; 
+
     }
 }
 
