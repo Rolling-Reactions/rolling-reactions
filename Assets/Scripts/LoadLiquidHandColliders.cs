a0fe1ab5269f7d7cef7b40f115203c4d8781a444
Aritra Bhakt <arre2barre@gmail.com>
2023-09-25 11:47:41 +0200

    not important liquid stuff

---
+++ Assets/Scripts/LoadLiquidHandColliders.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LoadLiquidHandColliders : MonoBehaviour
+{
+    GameObject player;
+    void Awake()
+    {
+
+    }
+}
